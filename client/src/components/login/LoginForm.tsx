0d0c3345517805d0e213f0a40b743258dfde89db
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-04-29 22:34:58 +0800

    update

---
+++ client/src/components/login/LoginForm.tsx
@@ -0,0 +1,34 @@
+import { User } from 'lucide-react';
+import Logo from "../../assets/logo-and-text.png"
+
+export default function LoginForm() {
+    return (
+        <>
+            <div className="flex flex-col justify-center min-h-screen py-10 px-5 sm:hidden">
+                <img src={Logo} alt="Ethos Logo" className="w-30 h-auto mb-15" />
+
+                <div className="flex flex-col gap-y-3 mb-10">
+                    <h2 className="text-primary-color text-xl">Welcome Back!</h2>
+                    <p className="font-semibold text-4xl">Log in to continue your ethical journey</p>
+                    <span className="text-sm text-white/70">Log your decisions. Understand your patterns. <br /> Grow with insight</span>
+                </div>
+
+                <div className="flex flex-col gap-y-3">
+                    <div className="flex flex-col gap-y-1">
+                        <span className="text-primary-color text-sm">Username</span>
+
+                        <div className="relative">
+                            <User className="absolute left-2 top-1/2 -translate-y-1/2 w-5 h-5 text-white/70" />
+
+                            <input
+                                type="text"
+                                placeholder="Enter your username"
+                                className="pl-9 pr-3 py-2 w-full rounded-md text-white placeholder-white/50 border border-white/70 focus:outline-none focus:border-primary-color"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,24 +1,15 @@
-import Logo from "../assets/logo-and-text.png"
+import { useEffect } from "react";
+import LoginForm from "../components/login/LoginForm";
 
 export default function LoginPage() {
+
+    useEffect(() => {
+        document.title = "Login - Ethos";
+    }, []);
+
     return(
         <>
-            <div className="flex flex-col justify-center min-h-screen py-10 px-5 sm:hidden">
-                <img src={Logo} alt="Ethos Logo" className="w-30 h-auto mb-15" />
-                
-                <div className="flex flex-col gap-y-3 mb-10">
-                    <h2 className="text-primary-color text-xl">Welcome Back!</h2>
-                    <p className="font-semibold text-4xl">Log in to continue your ethical journey</p>
-                    <span className="text-sm text-white/70">Log your decisions. Understand your patterns. <br /> Grow with insight</span>
-                </div>
-
-                <div className="flex flex-col gap-y-3">
-                    <div className="flex flex-col gap-y-1">
-                        <span className="text-primary-color text-sm">Username</span>
-                        <input type="text" placeholder="Enter your username" />
-                    </div>
-                </div>
-            </div>
+            <LoginForm />
         </>
     );
 }
