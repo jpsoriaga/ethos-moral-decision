1c4605b316d9b83bf5f4e5c52508c127b32581d1
Jaypee <patricksoriaga14@gmail.com>
2026-05-01 01:57:06 +0800

    add toast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import { ProtectedRoute, PublicRoute } from "./routes/ProtectedRoute";
 import DashboardPage from "./pages/DashboardPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          style: {
+            background: "#1c1c1c",
+            color: "#fff",
+            borderRadius: "12px",
+            border: "1px solid rgba(255, 255, 255, 0.08)",
+          },
+        }}
+      />
       <Routes>
         <Route path="/" element={<PublicRoute><LoginPage /></PublicRoute>} />
 
--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
-
+import toast from "react-hot-toast";
 
 export default function DashboardPage() {
-
     useEffect(() => {
         document.title = "Dashboard | Ethos";
     }, []);
+
+    toast.success("Successfully logged in!");
 
     return(
         <>
