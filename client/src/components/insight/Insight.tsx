61b4dc8cb3f0cd41db378708921242de781f9062
Jaypee <patricksoriaga14@gmail.com>
2026-05-02 04:02:58 +0800

    update

--- client/src/components/insight/Insight.tsx
+++ client/src/components/insight/Insight.tsx
@@ -1,10 +1,19 @@
 import HeroSection from "./HeroSection";
+import RiskCard from "./RiskCard";
 
 export default function Insight() {
     return(
         <>
-            <div className="w-full flex flex-col top-13 fixed h-auto p-5">
+            <div className="w-full flex flex-col top-13 fixed h-auto p-5 gap-y-5">
                 <HeroSection />
+                <div className="justify-between flex gap-x-3">
+                    <div className="w-1/2">
+                        <RiskCard />
+                    </div>
+                    <div className="w-1/2">
+                        
+                    </div>
+                </div>
             </div>
         </>
     );
